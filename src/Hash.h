23a75792a5ff761aabc76034dfef30515519e61d
csv610 <csv610@yahoo.com>
2026-04-20 21:36:34 +0530

    Migrate deprecated <ext/hash_map> to C++20 <unordered_map>

--- src/Hash.h
+++ src/Hash.h
@@ -1,29 +1,8 @@
 #ifndef HASH_INCLUDED
 #define HASH_INCLUDED
-#ifdef WIN32
-#include <hash_map>
-using stdext::hash_map;
-#else // !WIN32
-#include <ext/hash_map>
-using namespace __gnu_cxx;
+#include <unordered_map>
 
-namespace __gnu_cxx
-{
-  template<> struct hash<long long> {
-    size_t operator()(long long __x) const { return __x; }
-  };
-  template<> struct hash<const long long> {
-    size_t operator()(const long long __x) const { return __x; }
-  };
-  
-  
-  template<> struct hash<unsigned long long> {
-    size_t operator()(unsigned long long __x) const { return __x; }
-  };
-  template<> struct hash<const unsigned long long> {
-    size_t operator()(const unsigned long long __x) const { return __x; }
-  };
-}
-#endif // WIN32
+template< class Key , class T , class Hash = std::hash< Key > , class Pred = std::equal_to< Key > , class Alloc = std::allocator< std::pair< const Key , T > > >
+using hash_map = std::unordered_map< Key , T , Hash , Pred , Alloc >;
+
 #endif // HASH_INCLUDED
-
